fdb387ac8f7316e5b64e89019033cd415f8ac723
Arthuro1 <arthur.meteng@yahoo.com>
2026-06-25 00:02:19 +0200

    fix flag

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { polyfillCountryFlagEmojis } from 'country-flag-emoji-polyfill'
 import App from './App.jsx'
 import './index.css'
+
+// Injects a small "Twemoji Country Flags" web font so flag emojis render on
+// platforms (notably Windows) that don't support regional-indicator flags.
+polyfillCountryFlagEmojis()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
